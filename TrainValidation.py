loss calculation is wrong, debug

TrainValidation.py
@@ -1,5 +1,5 @@
-# from mylayer import MyLayer
-from smalllayer import MyLayer
+from mylayer import MyLayer
+# from smalllayer import MyLayer
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -49,9 +49,8 @@ def train_val(n_epoch, lr_input, dataset, training_idx, val_idx, L1lam, L2lam, r
             # namely the predicted category
             acc = train_out.max(1)[1].eq(data.y.view(-1).type(torch.long))
             train_batch_accs.append(acc.cpu().numpy())
-        trainbatch_mean = np.array(train_batch_accs).mean()
-        train_accs.append(trainbatch_mean)
-        train_loss.append(trainbatch_mean)
+        train_accs.append(np.array(train_batch_accs).mean())
+        train_loss.append(np.array(train_batch_loss).mean())
 
         model.eval()
         val_batch_acc = []

hamiltonian.py
@@ -93,7 +93,7 @@ class Hamiltonian(torch.utils.data.Dataset):
                 h_row = []
                 # len(orb_list) + 4 because 3 x,y,z coordinate
                 # of atom center in unit cell and the atom center itself
-                atom_feat = np.zeros((n_orb.squeeze(), 4), dtype=np.float32)
+                atom_feat = np.zeros((n_orb.squeeze(), 20), dtype=np.float32)
                 # atom_feat's dimension must be ?*20.
                 # orb_list is the possible name of the orbital
                 for a in range(atom_info[0].squeeze()):
@@ -102,10 +102,10 @@ class Hamiltonian(torch.utils.data.Dataset):
                         # atom number of each atom
                         atom_feat[j, -4: -1] = atom_info[2][a] + np.array(pos_list[i]) * cell_size
                         # -4:-1 is the coordinate of the atom w.r.t. the unit cell
-                        # elements_name = atom_info[7][0][a][j - (atom_info[-2][a][0] - 1)][0][0]
+                        elements_name = atom_info[7][0][a][j - (atom_info[-2][a][0] - 1)][0][0]
                         # elements_name should be orbital name here
                         # since it is the orbitals' names of each atom
-                        # atom_feat[j, orb_list[elements_name]] = 1
+                        atom_feat[j, orb_list[elements_name]] = 1
                         # mark orbitals' names of each atom to be 1
                 x_feat.append(atom_feat)
 

mylayer.py
@@ -5,27 +5,21 @@ from torch_scatter import scatter_mean, scatter_add
 from torch_geometric.nn import global_mean_pool
 
 class MyLayer(nn.Module):
-    def __init__(self):
+    def __init__(self, drop_p):
         super(MyLayer, self).__init__()
 
         ###################################################
         # following algorithm achieves 70% accruacy
-        self.linear1 = nn.Linear(4, 8)
-        # self.linear2 = nn.Linear(20, 64)
-        self.linear3 = nn.Linear(8, 16)
-        # self.linear4 = nn.Linear(64, 128)
-        self.linear5 = nn.Linear(16, 32)
-        # self.linear6 = nn.Linear(128, 256)
-        self.linear7 = nn.Linear(32, 2)
+        self.linear1 = nn.Linear(20, 32)
+        self.linear3 = nn.Linear(32, 64)
+        self.linear7 = nn.Linear(64, 2)
         # initialization to 0 or identity matrix
         # both achieve at least 60% of accuracy
-        # nn.init.zeros_(self.linear1.weight)
-        # nn.init.zeros_(self.linear2.weight)
-        # nn.init.zeros_(self.linear3.weight)
-        # nn.init.zeros_(self.linear4.weight)
-        # nn.init.zeros_(self.linear5.weight)
-        # nn.init.zeros_(self.linear6.weight)
-        # nn.init.zeros_(self.linear7.weight)
+        nn.init.zeros_(self.linear1.weight)
+        nn.init.zeros_(self.linear3.weight)
+        nn.init.zeros_(self.linear7.weight)
+        self.drop1 = nn.Dropout(p=drop_p)
+        self.drop2 = nn.Dropout(p=drop_p)
 
 
     def forward(self, x, edge, weight):
@@ -37,20 +31,17 @@ class MyLayer(nn.Module):
         out1 = F.leaky_relu(self.linear1(out1))
         out1 = scatter_mean(out1, row, dim=0)
         out1 = out1 + F.leaky_relu(self.linear1(x))
+        out1 = self.drop1(out1)
 
         out2 = (out1[col])*weight[:, None]
         out2 = F.leaky_relu(self.linear3(out2))
         out2 = scatter_mean(out2, row, dim=0)
         out2 = out2 + F.leaky_relu(self.linear3(out1))
+        out2 = self.drop2(out2)
 
-        out3 = (out2[col]) * weight[:, None]
-        out3 = F.leaky_relu(self.linear5(out3))
-        out3 = scatter_mean(out3, row, dim=0)
-        out3 = out3 + F.leaky_relu(self.linear5(out2))
+        out2 = global_mean_pool(out2, torch.zeros(out2.shape[0], dtype=torch.long).cuda())
+        out2 = self.linear7(out2)
 
-        out3 = global_mean_pool(out3, torch.zeros(out3.shape[0], dtype=torch.long).cuda())
-        out3 = self.linear7(out3)
+        out2 = F.log_softmax(out2, dim =1)
 
-        out3 = F.log_softmax(out3, dim =1)
-
-        return out3
+        return out2