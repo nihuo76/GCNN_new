0f81ec08d1f6de24e529226ec5009bf22e7e4744
Hexin <tug76276@temple.edu>
2019-08-04 14:27:21 -0400

    Add GGNN global pooling in my method

--- Peng_Chu.py
+++ Peng_Chu.py
@@ -10,9 +10,9 @@ class Net(nn.Module):
         # self.conv2 = GCNConv(16, dataset.num_classes, cached=True)
         self.conv1 = geonn.ChebConv(20, 32, K=1)
         self.conv2 = geonn.ChebConv(32, 64, K=2)
-        # self.conv3 = geonn.ChebConv(64, 2, K=2)
-        self.GGNNpooling = geonn.GlobalAttention(nn.Linear(64, 1))
-        self.linear1 = nn.Linear(64, 2)
+        self.conv3 = geonn.ChebConv(64, 2, K=2)
+        self.GGNNpooling = geonn.GlobalAttention(nn.Linear(2, 1))
+        # self.linear1 = nn.Linear(64, 2)
         # self.fc = nn.Linear(16 * data.num_nodes, 1)
 
     def forward(self, x, edge_index, attr):
@@ -21,9 +21,9 @@ class Net(nn.Module):
         # x = F.dropout(x, training=self.training)
         x = F.leaky_relu(self.conv2(x, edge_index, attr))
         # x = F.dropout(x, training=self.training)
-        # x = self.conv3(x, edge_index, attr)
+        x = self.conv3(x, edge_index, attr)
         x = self.GGNNpooling(x, torch.zeros(x.shape[0], dtype=torch.long).cuda())
-        x = self.linear1(x)
+        # x = self.linear1(x)
         # the following global pooling is in Peng's original method
         # x = global_mean_pool(x, torch.zeros(x.shape[0], dtype=torch.long).cuda())
         # x = self.fc(x.view(-1)).view((-1, 1))
--- mylayer.py
+++ mylayer.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch_scatter import scatter_mean, scatter_add
-from torch_geometric.nn import global_mean_pool
+from torch_geometric.nn import global_mean_pool, GlobalAttention
 
 class MyLayer(nn.Module):
     def __init__(self, drop_p):
@@ -12,6 +12,7 @@ class MyLayer(nn.Module):
         # following algorithm achieves 70% accruacy
         self.linear1 = nn.Linear(20, 32)
         self.linear3 = nn.Linear(32, 64)
+        self.GGNNpooling = GlobalAttention(nn.Linear(64, 1))
         self.linear7 = nn.Linear(64, 2)
         # initialization to 0 or identity matrix
         # both achieve at least 60% of accuracy
@@ -39,9 +40,10 @@ class MyLayer(nn.Module):
         out2 = out2 + F.leaky_relu(self.linear3(out1))
         out2 = self.drop2(out2)
 
-        out2 = global_mean_pool(out2, torch.zeros(out2.shape[0], dtype=torch.long).cuda())
+        out2 = self.GGNNpooling(out2, torch.zeros(out2.shape[0], dtype=torch.long).cuda())
+        # out2 = global_mean_pool(out2, torch.zeros(out2.shape[0], dtype=torch.long).cuda())
         out2 = self.linear7(out2)
 
-        out2 = F.log_softmax(out2, dim =1)
+        out2 = F.log_softmax(out2, dim=1)
 
         return out2
